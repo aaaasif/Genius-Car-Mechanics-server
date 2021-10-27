4c19c1a1919019f4d16e5cd3af50758998a34c71
aaaasif <alexasif4@gmail.com>
2021-10-27 09:36:04 +0600

    gitignore added

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ require('dotenv').config();
 const app = express();
 const port = 5000;
 
-const uri = "mongodb+srv://aaaasif:<password>@cluster0.kz6u6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kz6u6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get ('/', (req, res) =>{
